7bd24420a0bf3d7a66b5c3c6724f88f56c6dc1af
Mihai Balan <mihai.balan@esolutions.ro>
2023-10-03 23:38:39 +0300

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -19,11 +19,8 @@ export default async function IndexPage() {
     <div className="divide-y divide-gray-200 dark:divide-gray-700">
       <div className="space-y-2 pt-6 pb-8 md:space-y-5">
         <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-          Aventuri din Muntii Nostrii
+          Trekking Journal
         </h1>
-        <h3 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-          cu tovarasii meiii
-        </h3>
       </div>
 
       <ul>
