3ada561cce1012cdf3a43749f89c0ca3294c765b
Mihai Balan <mihai.balan@esolutions.ro>
2023-10-10 13:47:40 +0300

    mvp prm

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,18 +1,22 @@
 import Link from "next/link";
 import Themebutton from "./ThemeButton";
+import style from "../styles/Navbar.module.scss";
 
 export default function Navbar() {
   return (
-    <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 sticky top-0 bg-inherit">
-      <div className="flex justify-between h-16">
-        <div className="flex justify-between items-center w-full">
-          <Link href="/">
-            <h1 className="text-2xl font-medium">
-              Primates <span className="text-teal-500">Prime.</span>
-            </h1>
-          </Link>
-
-          <Themebutton />
+    <div className="z-50 bg-inherit sm:px-6 lg:px-8 sticky top-0">
+      <div className=" max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+          <div className="flex justify-between items-center w-full">
+            <Link href="/">
+              <h1 className="text-2xl font-medium">
+                <span className={style.logo}>
+                  <span className="text-teal-500">Primates</span>
+                </span>
+              </h1>
+            </Link>
+            <Themebutton />
+          </div>
         </div>
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -24,13 +24,47 @@ export default async function IndexPage() {
   return (
     <div className=" divide-gray-200 dark:divide-gray-700 w-full">
       <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-          Trekking Journal
-        </h1>
-        <h5 className="mb-2 text-2xl max-w-2xl tracking-tight text-gray-900 dark:text-white">
-          A group of dumb friends wondering around Romania's most pitoresq
-          landscapes.
-        </h5>
+        <div className="mx-auto max-w-2xl py-36 sm:py-48 lg:py-42">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight  dark:text-teal-500 sm:text-6xl">
+              Trekking Journal
+            </h1>
+            <p className="mt-6 text-lg leading-8 dark:text-gray-200">
+              A group of dumb friends wondering around Romania's most pitoresq
+              landscapes. Get inspired to do your own or follow us to see how
+              we're doing.
+            </p>
+            {/* <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="#"
+                className="rounded-md bg-teal-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-white 
+                hover:text-teal-500
+                focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-500"
+              >
+                Get started
+              </a>
+              <a
+                href="#"
+                className="text-sm font-semibold leading-6 text-teal-500"
+              >
+                Learn more <span aria-hidden="true">→</span>
+              </a>
+            </div> */}
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="absolute -z-10 inset-x-0 transform-gpu overflow-hidden blur-3xl xl:-top-12 sm:-top-20"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#48b248] to-teal-500 opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        />
       </div>
 
       <ul className="grid xl:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-20 w-full pb-10">
